1d03eb5413b42ad15281d73ee35b0848effef557
zhangheng <1515452760@qq.com>
2022-09-28 23:06:23 +0800

    feat:完成开发~~

---
+++ README.md
@@ -0,0 +1,25 @@
+依赖安装
+
+```shell
+  yarn
+```
+
+没有 yarn 工具可以使用
+
+```shell
+ npm install yarn -g
+```
+
+进行安装
+
+dev 运行
+
+```shell
+  yarn dev
+```
+
+打包构建
+
+```shell
+  yarn build
+```
--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,5 +1,6 @@
-import { Button, Form, Input, message } from "antd";
-import { useState } from "react";
+import { Button, Form, Input, Tooltip } from "antd";
+import { InfoCircleOutlined } from "@ant-design/icons";
+import { useCallback, useState } from "react";
 import { updateCookie } from "../../request/api";
 import { successMsg, errorMsg } from "../../utils";
 
@@ -9,12 +10,18 @@ export interface SubmitValues {
   newCookie: string;
 }
 
+const text = `
+  目前需要自行登录 m.jd.com 打开控制台找到cookie中的一段字符串，
+  格式如下：pt_key=xxx; pt_pin=xxx;
+`;
+
 const form = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [form] = Form.useForm<SubmitValues>();
   const onFinish = async (values: SubmitValues) => {
     setIsLoading(true);
     try {
-      const res = await updateCookie(values);
+      await updateCookie(values);
       // 判断响应内容
       successMsg("更新成功");
     } catch (error) {
@@ -24,8 +31,13 @@ const form = () => {
     }
   };
 
+  const handleClickReset = useCallback(() => {
+    form.resetFields();
+  }, [form]);
+
   return (
     <Form
+      form={form}
       labelCol={{
         span: 6,
       }}
@@ -77,7 +89,21 @@ const form = () => {
           },
         ]}
       >
-        <Input.TextArea rows={3} />
+        <Input.TextArea
+          placeholder="cookie格式  pt_key=xxxxx; pt_pin=xxxxx;"
+          rows={3}
+        />
+        <div
+          style={{
+            position: "absolute",
+            bottom: 0,
+            right: "-18px",
+          }}
+        >
+          <Tooltip placement="topRight" title={text}>
+            <InfoCircleOutlined />
+          </Tooltip>
+        </div>
       </Form.Item>
 
       <Form.Item
@@ -86,9 +112,28 @@ const form = () => {
           justifyContent: "center",
         }}
       >
-        <Button loading={isLoading} type="primary" htmlType="submit">
-          更新
-        </Button>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            style={{
+              margin: "0 5px",
+            }}
+            loading={isLoading}
+            type="primary"
+            htmlType="submit"
+          >
+            更新
+          </Button>
+          <Button type="primary" onClick={handleClickReset}>
+            重置
+          </Button>
+        </div>
       </Form.Item>
     </Form>
   );
